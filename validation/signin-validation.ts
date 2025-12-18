8edd747983b73c4d25ce46ebcc27c7a0e6e8138d
nour jouini <nour.edaher.jouini@gmail.com>
2025-12-18 11:50:57 +0100

    auth context and formik

---
+++ validation/signin-validation.ts
@@ -0,0 +1,14 @@
+import * as yup from 'yup';
+
+export const SignInSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Merci d'entrer une adresse email valide")
+    .required("L'email est obligatoire"),
+  password: yup
+    .string()
+    .min(6, 'Le mot de passe doit contenir au moins 6 caractères')
+    .required('Le mot de passe est obligatoire'),
+});
+
+
